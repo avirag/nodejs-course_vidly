f895dfd73e76b9231be0272efc7cc66a9e08dee6
Anita Virag <anitavirag.dev@gmail.com>
2019-01-18 21:48:40 +0100

    modeling relationships

--- routes/customers.js
+++ routes/customers.js
@@ -1,26 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Joi = require("joi");
-const mongoose = require('mongoose');
-
-const Customer = new mongoose.model('Customer', new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  },
-  isGold: {
-    type: Boolean,
-    default: false
-  },
-  phone: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  }
-}));
+const {Customer, validate} = require('../models/customer');
 
 router.get("/", async (req, res) => {
   const customers = await Customer.find().sort('name');
@@ -28,7 +8,7 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const { error } = validateCustomer(req.body);
+  const { error } = validate(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
@@ -43,7 +23,7 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  const { error } = validatecustomer(req.body);
+  const { error } = validate(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
@@ -79,21 +59,5 @@ router.get("/:id", async (req, res) => {
 
   res.send(customer);
 });
-
-function validateCustomer(customer) {
-  const schema = {
-    name: Joi.string()
-      .min(5)
-      .max(50)
-      .required(),
-    phone: Joi.string()
-      .min(5)
-      .max(50)
-      .required(),
-    isGold: Joi.boolean()
-  };
-
-  return Joi.validate(customer, schema);
-}
 
 module.exports = router;
